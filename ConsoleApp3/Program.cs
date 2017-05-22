0edb3b2b3bb2d82d1a5ecc2fa083bc9c9cf3a48a
Surya Venkata Hanumantha Vajjala <hvsv_nkumar@hotmail.com>
2017-05-21 23:49:15 -0500

    updated

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,7 @@
     using System.Text;
     using System.Threading.Tasks; 
     using System.Linq.Dynamic;
-    namespace ConsoleApp3
+    namespace Console1
     {
         class Program
         {
